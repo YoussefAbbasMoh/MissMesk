745711ee4928c86e6c8bff9e75b96547cd15ba0d
BabaVoos <mudarabdulhadi@gmail.com>
2025-04-28 11:10:04 +0300

    Add app pallete and app text styles calasses

---
+++ lib/core/theming/app_pallete.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/widgets.dart';
+
+class AppPallete {
+  static const Color primaryColor = Color(0xFFC8A254);
+  static const Color blackColor = Color(0xFF000000);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color greyColor = Color(0xFF4A4A4A);
+  static const Color lightGreyColor = Color(0xFFD1D1D1);
+}
---
+++ lib/core/theming/app_text_styles.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle font29BlackSemiBold = TextStyle(
+    fontSize: 29,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+
+  static const TextStyle font21WhiteRegular = TextStyle(
+    fontSize: 21,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+
+  static const TextStyle font18BlackRegular = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  static const TextStyle font16BlackRegular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Colors.black,
+  );
+
+  static const TextStyle font24WhiteSemiBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    color: Colors.white,
+  );
+
+  static const TextStyle font20BlackSemiBold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: Colors.black,
+  );
+}
