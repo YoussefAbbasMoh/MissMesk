18d0d15ed3228301cc6e3c3381b4afa52cd97b5c
BabaVoos <mudarabdulhadi@gmail.com>
2025-05-25 16:49:49 +0300

    feat(extensions) Add StringExtensions with capitalize and isEmail methods for enhanced string manipulation

--- lib/core/utils/extensions.dart
+++ lib/core/utils/extensions.dart
@@ -12,3 +12,17 @@ extension QueryParamsExtension on BuildContext {
     return uri.queryParameters[key];
   }
 }
+
+extension StringExtensions on String {
+  String capitalize() {
+    if (isEmpty) return this;
+    return '${this[0].toUpperCase()}${substring(1)}';
+  }
+
+  bool isEmail () {
+    final emailRegex = RegExp(
+      r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+    );
+    return emailRegex.hasMatch(this);
+  }
+}
