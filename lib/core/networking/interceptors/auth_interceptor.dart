8a467b6abcd0d5323a16d1c80a274737d078035b
BabaVoos <mudarabdulhadi@gmail.com>
2025-05-26 16:38:25 +0300

    feat(interceptors) Enhance AuthInterceptor to handle token errors and redirect to login

--- lib/core/networking/interceptors/auth_interceptor.dart
+++ lib/core/networking/interceptors/auth_interceptor.dart
@@ -1,4 +1,7 @@
 import 'package:dio/dio.dart';
+import 'package:go_router/go_router.dart';
+import 'package:miss_misq/core/routing/app_router.dart';
+import 'package:miss_misq/core/routing/routes.dart';
 
 class AuthInterceptor extends Interceptor {
   final Future<String?> Function() getToken;
@@ -15,5 +18,18 @@ class AuthInterceptor extends Interceptor {
       options.headers['token'] = 'Misk $token';
     }
     return handler.next(options);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    if (err.response?.data['message'] == 'Token Invalid') {
+      AppRouter.rootNavigatorKey.currentContext?.go(AppRoutes.login);
+    } else if (err.response?.data['message'] == 'Token Expired') {
+      AppRouter.rootNavigatorKey.currentContext?.go(AppRoutes.login);
+    } else if (err.response?.data['message'] ==
+        'You are not authorized to access this route') {
+      AppRouter.rootNavigatorKey.currentContext?.go(AppRoutes.login);
+    }
+    return handler.next(err);
   }
 }
